f4472f58d92045b0d61b8547b481e3c6c244d2d2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-02 18:35:41 +0900

    Delete data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-from orjson import loads
-
-class Config(TypedDict):
-    host: str
-    port: int
-
-with open("config.json", "r") as f:
-    config: Config = loads(f.read())
