95c2b4c70b30df7d75b86362fa4510ba8ecb92c6
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-04 21:12:49 +0900

    Update utils.py

--- blueprints/v2/utils.py
+++ blueprints/v2/utils.py
@@ -1,1 +1,23 @@
+from sanic import WebSocket
 
+import asyncio
+
+
+class HeartBeat:
+    def __init__(self, ws):
+        self.ws = ws
+        self.open: bool = True
+
+    async def send_heartbeat(self):
+        await self.ws.send(dumper('heartbeat', {"unix_time": time()}))
+
+    async def start(self):
+        while self.open:
+            try:
+                payload = loads(zlib.decompress(await asyncio.wait_for(self.ws.recv(), timeout=30)))
+                if payload["type"] == "heartbeat":
+                    await self.send_heartbeat()
+                    await asyncio.sleep(10)
+            except asyncio.TimeoutError:
+                await manager.disconnect(ws)
+                self.open = False
