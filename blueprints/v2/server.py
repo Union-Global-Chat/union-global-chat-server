7bbeccdf55304b9999ff17a4d77968156d48d1c8
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-04 21:12:21 +0900

    Update server.py

--- blueprints/v2/server.py
+++ blueprints/v2/server.py
@@ -37,25 +37,6 @@ def dumper(type: str, data: dict=None, *, success: bool=True, message: str=None)
     return zlib.compress(dumps(payload))
 
 wss = []
-
-class HeartBeat:
-    def __init__(self, ws):
-        self.ws = ws
-        self.open: bool = True
-
-    async def send_heartbeat(self):
-        await self.ws.send(dumper('heartbeat', {"unix_time": time()}))
-
-    async def start(self):
-        while self.open:
-            try:
-                payload = loads(zlib.decompress(await asyncio.wait_for(self.ws.recv(), timeout=30)))
-                if payload["type"] == "heartbeat":
-                    await self.send_heartbeat()
-                    await asyncio.sleep(10)
-            except asyncio.TimeoutError:
-                await manager.disconnect(ws)
-                self.open = False
             
 @lib.loop(10)
 async def status_check():
