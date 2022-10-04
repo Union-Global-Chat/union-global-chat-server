567c2560174ace64c4f15e99088954917af7bd4d
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-04 21:50:50 +0900

    Create data.py

---
+++ blueprints/v2/data.py
@@ -0,0 +1,21 @@
+from utils import DatabaseManager
+
+
+class DataManager(DatabaseManager):
+    def __init__(self, app: Sanic):
+        self.pool = app.ctx.pool
+
+    async def prepare_table(self, cursor):
+        await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Message(
+                Channel JSON,
+                Author JSON,
+                Guild JSON
+                Message JSON
+            );""")
+
+    async def create_message(self, cursor, channel, author, guild, message):
+        await cursor.execute(
+            "INSERT INTO Message VALUES (%s, %s, %s, %s);",
+            (channel, author, guild message)
+        )
