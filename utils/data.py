085d71b273ce905a9f13e8455b52c85b731794d7
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-04 21:34:37 +0900

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,6 @@
 from aiomysql import Pool
 
-from inspect import getmembers, isfunction
+from inspect import getmembers, isfunction, iscoroutinefunction
 from functools import wraps
 
 
@@ -9,10 +9,12 @@ class DatabaseManager:
 
     def __init_subclass__(cls):
         for name, func in getmembers(cls, isfunction):
-            if not name.startswith("_"):
+            if not name.startswith("_") and iscoroutinefunction(func):
                 @wraps(func)
-                async def new(*args, **kwargs):
-                    async with cls.pool.acquire() as conn:
+                async def new(self, *args, **kwargs):
+                    if "cursor" in kwargs:
+                        return await func(cur, *args, **kwargs)
+                    async with self.pool.acquire() as conn:
                         async with conn.cursor() as cur:
                             return await func(cur, *args, **kwargs)
                 setattr(name, cls, func)
