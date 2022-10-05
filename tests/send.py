935ce8a3936cfbafc0b820139d1f003959f3b699
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-05 15:57:40 +0900

    Update send.py

--- tests/send.py
+++ tests/send.py
@@ -4,7 +4,7 @@ token = "ik8J6D4qRggWkWBVzti07feetHNh2USP"
 
 def request(method: str, url: str, *args, **kwargs):
     kwargs["headers"] = {"Authorization": "Bearer {}".format(token)}
-    return requests.request(method, "http://localhost:8080/api/v1" + url, *args, **kwargs)
+    return requests.request(method, "http://localhost:8080/api/v2" + url, *args, **kwargs)
 
 
 def send():
