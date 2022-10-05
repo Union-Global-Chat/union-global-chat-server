1d4f86146c24277d1b5fd688b5aacd8a6843282e
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-05 15:02:00 +0900

    Update utils.py

--- blueprints/v2/utils.py
+++ blueprints/v2/utils.py
@@ -1,7 +1,25 @@
 from sanic import Websocket
+import jwt
+from jwt.exceptions import InvalidSignatureError
 
 import asyncio
+from functools import wrap
 
+from data import CONFIG
+from types import JwtDecodedType
+
+
+def authorized(func):
+    @wraps(func)
+    async def decorated_func(request, *args, **kwargs):
+        try:
+            user: JwtDecodedType = jwt.decode(request.token, CONFIG["secret_key"], algorithms="HS256")
+        except InvalidSignatureError:
+            return json(message="Authorized failed", status=401)
+        else:
+            return await func(request, user, *args, **kwargs)
+    return decorated_func
+        
 
 class HeartBeat:
     def __init__(self, ws: Websocket):
