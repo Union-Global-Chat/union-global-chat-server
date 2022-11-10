b69aada24a58bd43173a8e18f09b2ba02c8082f7
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-11 07:48:55 +0900

    fix: args

--- cli.py
+++ cli.py
@@ -1,12 +1,12 @@
 from data import CONFIG
+from argparse import ArgumentParser
 
 import jwt
 import pymysql
 
 
-def main():
+def main(bot_id, bot_name):
     connection = pymysql.connect(**CONFIG["mysql"])
-    bot_id, bot_name = input("Bot's Id: "), input("Bot's Name: ")
     with connection:
         with connection.cursor() as cur:
             cur.execute(
@@ -16,4 +16,16 @@ def main():
     print(jwt.encode({"id": bot_id, "username": bot_name}, CONFIG["secret_key"]))
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("id", help="Bot's id")
+    parser.add_argument("name", help="Bot's name")
+    args = parser.parse_args()
+    if not args.id:
+        bot_id = input("Bot's Id: ")
+    else:
+        bot_id = args.id
+    if not args.name:
+        bot_name = input("Bot's Name: ")
+    else:
+        bot_name = args.name
+    main(bot_id, bot_name)
